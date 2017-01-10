5bc208945fcca6487c4345e3daf1b88a7f212beb
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-01-10 14:48:50 +0100

    Add server: Impl basic HTTP server

---
+++ server/index.js
@@ -0,0 +1,20 @@
+var http = require( 'http' )
+var os = require( 'os' )
+
+var server = http.createServer()
+
+server.once( 'listening', function() {
+  console.log( 'listening on', this.address() )
+})
+
+server.on( 'request', function( req, res ) {
+  if( req.url === '/' ) {
+    res.write( JSON.stringify( { host: os.hostname() }, null, 2 ) )
+    res.end()
+  } else {
+    res.statusCode = 404
+    res.end()
+  }
+})
+
+server.listen( 8080 )

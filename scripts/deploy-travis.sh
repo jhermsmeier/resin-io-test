a2aefc030b09dd0cf097563bbf88efb769fbc440
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-01-10 16:17:54 +0100

    Update .travis.yml: Move SSH setup and fix script path

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -1,12 +1,4 @@
-#!/usr/bin/env sh
-
-# Decrypt deployment key
-openssl aes-256-cbc -K $encrypted_d95c70ccd684_key -iv $encrypted_d95c70ccd684_iv -in .travis/deploy_resin_ed25519.enc -out .travis/deploy_resin_ed25519 -d
-# Start ssh-agent
-eval "$(ssh-agent -s)"
-# Add deploy key
-chmod 600 .travis/deploy_resin_ed25519
-ssh-add .travis/deploy_resin_ed25519
+#!/usr/bin/env bash
 
 # Deploy RPi1
 git remote add rpi1 jhermsmeier@git.resin.io:jhermsmeier/rpi1.git

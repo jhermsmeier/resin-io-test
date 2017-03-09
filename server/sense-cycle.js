364e0324fd28e2a8c5d31952a3ba2dc315b53ef2
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-03-09 16:19:35 +0100

    Update sense-cycle: Don't blow up if it doesnt work & log

--- server/sense-cycle.js
+++ server/sense-cycle.js
@@ -1,33 +1,78 @@
-var sense = require( 'sense-hat-led' )
+var fs = require( 'fs' )
+var path = require( 'path' )
+var sense = null
 
-var R = [ 255,   0,   0 ]
-var X = [ 255, 255, 255 ]
-var _ = [   0,   0,   0 ]
+var logfile = path.join( __dirname, '..', 'sense-hat.log' )
 
-var blankScreen = [
-  _, _, _, _, _, _, _, _,
-  _, _, _, _, _, _, _, _,
-  _, _, _, _, _, _, _, _,
-  _, _, _, _, _, _, _, _,
-  _, _, _, _, _, _, _, _,
-  _, _, _, _, _, _, _, _,
-  _, _, _, _, _, _, _, _,
-  _, _, _, _, _, _, _, _,
-]
+function log( value ) {
+  if( value instanceof Error ) {
+    value = {
+      time: new Date(),
+      event: 'error',
+      error: {
+        type: value.constructor.name,
+        message: value.message,
+        code: value.code,
+        stack: value.stack,
+      }
+    }
+  }
+  var line = JSON.stringify( value, null, 2 ) + '\n'
+  fs.writeFile( logfile, line, { flag: 'a' }, function( error ) {
+    if( error ) console.log( error )
+  })
+}
 
-var questionMark = [
-  _, _, _, R, R, _, _, _,
-  _, _, R, _, _, R, _, _,
-  _, _, _, _, _, R, _, _,
-  _, _, _, _, R, _, _, _,
-  _, _, _, R, _, _, _, _,
-  _, _, _, R, _, _, _, _,
-  _, _, _, _, _, _, _, _,
-  _, _, _, R, _, _, _, _,
-]
+function cycleScreen() {
 
-setInterval( function() {
+  var R = [ 255,   0,   0 ]
+  var X = [ 255, 255, 255 ]
+  var _ = [   0,   0,   0 ]
+
+  var blankScreen = [
+    _, _, _, _, _, _, _, _,
+    _, _, _, _, _, _, _, _,
+    _, _, _, _, _, _, _, _,
+    _, _, _, _, _, _, _, _,
+    _, _, _, _, _, _, _, _,
+    _, _, _, _, _, _, _, _,
+    _, _, _, _, _, _, _, _,
+    _, _, _, _, _, _, _, _,
+  ]
+
+  var questionMark = [
+    _, _, _, R, R, _, _, _,
+    _, _, R, _, _, R, _, _,
+    _, _, _, _, _, R, _, _,
+    _, _, _, _, R, _, _, _,
+    _, _, _, R, _, _, _, _,
+    _, _, _, R, _, _, _, _,
+    _, _, _, _, _, _, _, _,
+    _, _, _, R, _, _, _, _,
+  ]
+
   var image = Math.random() >= 0.5 ?
     blankScreen : questionMark
+
+  log({
+    time: new Date(),
+    event: 'cycle',
+    image: image === questionMark ?
+      'questionMark' : 'blank'
+  })
+
   sense.setPixels( image )
-}, 1000 )
+
+}
+
+log({ time: new Date(), event: 'start' })
+
+try {
+  sense = require( 'sense-hat-led' )
+} catch( error ) {
+  log( error )
+}
+
+if( sense != null ) {
+  setInterval( cycleScreen, 1000 )
+}
